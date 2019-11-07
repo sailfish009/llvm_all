174e12ec188ff2a589533fbf5c8b62b531d2da41
sailfish009 <sailfish009@gmail.com>
2019-11-07 22:11:03 +0000

    fix for ubuntu 19.10

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 mkdir build 
 cd build
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DLLVM_BUILD_DOCS=OFF -DCMAKE_INSTALL_PREFIX=/usr ../llvm
+CC=gcc-8 CXX=g++-8 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DLLVM_BUILD_DOCS=OFF -DCMAKE_INSTALL_PREFIX=/usr ../llvm
 make -j 8
 sudo make install/strip
