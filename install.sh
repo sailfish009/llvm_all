a643ce04ebd410b79e378921cdc5e9e0ef419656
sailfish009 <sailfish009@gmail.com>
2020-02-26 23:50:11 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,14 @@
 
 mkdir build 
 cd build
-CC=gcc-8 CXX=g++-8 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DLLVM_BUILD_DOCS=OFF -DCMAKE_INSTALL_PREFIX=/usr ../llvm
+CC=gcc-8 CXX=g++-8 cmake -G "Unix Makefiles" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DLLVM_TARGETS_TO_BUILD="host;AMDGPU" \
+    -DLLVM_BUILD_LLVM_DYLIB=ON \
+    -DLLVM_LINK_LLVM_DYLIB=ON \
+    -DLLVM_ENABLE_FFI=ON \
+    -DLLVM_ENABLE_RTTI=ON \
+    -DLLVM_BUILD_DOCS=OFF \
+    -DCMAKE_INSTALL_PREFIX=/usr ../llvm
 make -j 8
 sudo make install/strip
