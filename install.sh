d11da4786e59742ee793475d01f84e827417e481
sailfish009 <sailfish009@gmail.com>
2020-02-26 14:53:40 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ mkdir build
 cd build
 CC=gcc-8 CXX=g++-8 cmake -G "Unix Makefiles" \
     -DCMAKE_BUILD_TYPE=Release \
-    -DLLVM_TARGETS_TO_BUILD="host;AMDGPU" \
+    -DLLVM_TARGETS_TO_BUILD="host;AMDGPU;BPF" \
     -DLLVM_BUILD_LLVM_DYLIB=ON \
     -DLLVM_LINK_LLVM_DYLIB=ON \
     -DLLVM_ENABLE_FFI=ON \
